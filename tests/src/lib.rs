e5377eb9e46cce2ae916f3b9ec21b163bef23b3b
Michael Ilyin <milyin@gmail.com>
2025-07-02 11:38:40 +0200

    obsolete tests removed

--- tests/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Integration test crate for prebindgen
-//! 
-//! This crate contains tests that depend on prebindgen-proc-macro functionality.
-//! It's separated from the main crate to allow proper testing of proc-macro features.
-
-pub mod test_structures;
