e424da7b2267ea3f0818672a5704f0acde279820
Michael Ilyin <milyin@gmail.com>
2025-08-17 22:29:25 +0200

    make get_project_root return Result

--- examples/example-ffi/build.rs
+++ examples/example-ffi/build.rs
@@ -1,3 +1,10 @@
 fn main() {
     prebindgen::init_prebindgen_out_dir();
+
+    // In the complex cases involving code generation, the FFI crate may need
+    // access to the actual Cargo.lock file. Demonstrate how to obtain it.
+    let cargo_lock_path = prebindgen_project_root::get_project_root()
+        .unwrap_or_else(|e| panic!("Failed to determine workspace root: {}", e))
+        .join("Cargo.lock");
+    prebindgen::trace!("project's Cargo.lock is {}", cargo_lock_path.display());
 }
