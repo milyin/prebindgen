3ebd44cd11b0cb260c0bfbb41e43f2d65c95a147
milyin <milyin@users.noreply.github.com>
2026-08-27 02:05:26 +0000

    Remove preview for PR 585 🛬

--- pr-preview/pr-585/doc/trait.impl/core/iter/traits/collect/trait.FromIterator.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["prebindgen_registry",[["impl&lt;A: <a class=\"trait\" href=\"prebindgen_registry/write/trait.RustArtifact.html\" title=\"trait prebindgen_registry::write::RustArtifact\">RustArtifact</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;A&gt; for <a class=\"struct\" href=\"prebindgen_registry/write/struct.Assembly.html\" title=\"struct prebindgen_registry::write::Assembly\">Assembly</a>&lt;A&gt;",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[548]}
