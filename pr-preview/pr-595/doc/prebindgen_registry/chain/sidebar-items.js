db142ae728bbe449840c4746d9c75d4fef6b542e
milyin <milyin@users.noreply.github.com>
2026-08-27 08:17:30 +0000

    Deploy preview for PR 595 🛫

---
+++ pr-preview/pr-595/doc/prebindgen_registry/chain/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Call","Choice","ChoiceArm","ChoicePart","Invoke","Optional","Product","ProductPart","Rendered","RenderedInvokePart","Sequence","TupleChoice","TupleProduct"],"trait":["Chain","Child","ChoiceBridge","InvokeBridge","InvokePart","OptionalBridge","ProductBridge","SequenceBridge","Source"]};
