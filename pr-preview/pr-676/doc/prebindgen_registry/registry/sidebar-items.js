fd7aae07838323a2313cca9e4c8638d9f2ba0a78
milyin <milyin@users.noreply.github.com>
2026-09-02 02:43:42 +0000

    Deploy preview for PR 676 🛫

--- pr-preview/pr-676/doc/prebindgen_registry/registry/sidebar-items.js
+++ pr-preview/pr-676/doc/prebindgen_registry/registry/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Requirement","ScanError","WriteRustError"],"struct":["Answer","Building","Decompositions","DuplicateNameError","NotExpressibleEntry","Registry","RegistryBuilder","TypeKey","TypeKeyParseError"],"trait":["Conversions"],"type":["Crossing"]};
+window.SIDEBAR_ITEMS = {"enum":["Requirement","ScanError","WriteRustError"],"struct":["Building","Decompositions","DuplicateNameError","NotExpressibleEntry","Registry","RegistryBuilder","TypeKey","TypeKeyParseError"],"trait":["Conversions"],"type":["Crossing"]};
