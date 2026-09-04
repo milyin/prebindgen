783027169bad8394e94f3e3c5180b0db12cc0984
milyin <milyin@gmail.com>
2026-09-04 22:02:54 +0000

    Deploying to gh-pages from @ milyin/prebindgen@65e18f6ab8624e6a0103b236f68da79081963d8c 🚀

--- doc/prebindgen_registry/fold/sidebar-items.js
+++ doc/prebindgen_registry/fold/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["FoldError"],"struct":["Folding"],"trait":["FoldPolicy"]};
+window.SIDEBAR_ITEMS = {"enum":["FoldError","UnfoldViewError"],"struct":["Coverage","Folding"],"trait":["FoldPolicy","UnfoldPolicy"]};
