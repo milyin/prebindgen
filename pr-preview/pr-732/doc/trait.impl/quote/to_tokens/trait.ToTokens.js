da508191aba12095ff4f379fe900e789fab799d9
milyin <milyin@users.noreply.github.com>
2026-09-10 23:13:49 +0000

    Remove preview for PR 732 🛬

--- pr-preview/pr-732/doc/trait.impl/quote/to_tokens/trait.ToTokens.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["prebindgen",[["impl <a class=\"trait\" href=\"https://docs.rs/quote/1.0.47/quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"struct\" href=\"prebindgen/struct.TargetTriple.html\" title=\"struct prebindgen::TargetTriple\">TargetTriple</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[304]}
